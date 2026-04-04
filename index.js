088619fa3e169ac9ca00f346d0547573811c5e22
victoriawanko-create. <victoriawanko@gmail.com.>
2026-04-04 12:17:25 -0400

    final draft

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function openMenu () {
     document.body.classList += " menu--open"
 }
 
-function openMenu () {
-    document.body.classList.remove ('menu--open')
+function closeMenu () {
+    document.body.classList.remove('menu--open')
 }
